abe3f5053400221657044bba6eb692151764b323
guest_pc <guest_pc@MSI>
2021-08-22 02:32:36 +0700

    Add project files.

---
+++ Week02_2/Source.cpp
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main() {
+	int n, i, j;
+	scanf("%d", &n);
+	for (int i = 1; i <= (2 * n - 1); i++) {
+		if (i < n) {
+			for (int j = 1; j <= (2 * n - 1); j++) {
+				if (j >= n - (1 * i - 1) && j <= n + (1 * i - 1)) {
+					printf("*");
+				}
+				else {
+					printf(" ");
+				}
+			}
+			printf("\n");
+		}
+		else if (i == n) {
+			for (int j = 1; j <= (2 * n - 1); j++) {
+				printf("*");
+			}
+			printf("\n");
+		}
+		else {
+			for (int j = 1; j <= (2 * n - 1); j++) {
+				if (j > (i - n) && j < (2 * n - 1) + 1 - (i - n)) {
+					printf("*");
+				}
+				else {
+					printf(" ");
+				}
+			}
+			printf("\n");
+		}
+	}
+	return 0;
+}
